c2a6158244f1377b720dcb59beaa678d06038d6a
Vishal Pandey <bishalpandey2001@gmail.com>
2024-03-01 22:58:25 +0530

    Update main.js

---
+++ 01. Beginners/02. Datatypes and Variables/02. Datatypes/Tutorial/main.js
@@ -0,0 +1,58 @@
+// --------------------------------------------
+/* NUMBERS */
+
+// Javascript has one number type: Positive, Negative, Whole, Decimal
+const randomNumber = 10200;
+const favNumber = 21;
+let num = 2;
+
+// Basic Maths
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2/2);
+console.log(2%2);
+console.log(2**2);
+console.log(num++);
+console.log(num--);
+console.log(++num);
+console.log(--num);
+
+// --------------------------------------------
+/* BOOLEANS */
+let isCodingFun = true;
+let isDebuggingNeedsAlways = false;
+
+console.log(isCodingFun);
+console.log(isDebuggingNeedsAlways);
+
+// --------------------------------------------
+/* RELATIONAL OPERATORS */
+
+/* 
+1. Greater than: >
+2. Less than: <
+3. Greater Than or equal to: >=
+4. Less Than or equal to: <=
+*/
+
+console.log(10 > 10);
+console.log(10 < 10);
+console.log(10 >= 10);
+console.log(10 <= 10);
+
+/* EQUALITY OPERATORS */
+/* 
+1. === : strict equality (Type + Value)
+2. !== : strict non-equality (Type + Value)
+3. ==  : Lose equality (values)
+4. !=  : Lose equality (values)
+*/
+
+console.log(10 === 10);
+console.log(10 !== 10);
+console.log(10 == 10);
+console.log(10 != 10);
+
+// --------------------------------------------
+
