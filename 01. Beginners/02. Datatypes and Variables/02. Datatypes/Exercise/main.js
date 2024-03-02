e34107d05d70bc2123d3b85c463fc4ece4899950
Vishal Pandey <bishalpandey2001@gmail.com>
2024-03-02 15:59:16 +0530

    files changed

---
+++ 01. Beginners/02. Datatypes and Variables/02. Datatypes/Exercise/main.js
@@ -0,0 +1,25 @@
+/* NUMBERS */
+let firstFavNum = 3;
+let secondFavNum = 29;
+let addNum = firstFavNum + secondFavNum;
+let subNum = firstFavNum - secondFavNum;
+let mulNum = firstFavNum * secondFavNum;
+let divNum = firstFavNum / secondFavNum;
+let checkNum = firstFavNum % secondFavNum;
+let power = firstFavNum ** secondFavNum;
+
+console.log(`Addition: ${addNum}`);
+console.log(`Substraction: ${subNum}`);
+console.log(`Multiplication: ${mulNum}`);
+console.log(`Division: ${divNum}`);
+console.log(`Mod Division: ${checkNum}`);
+console.log(`Power: ${power}`);
+
+/* BOOLEANS */
+let isJsProgrammingLanguage = true;
+let isJsHard = false;
+let favNumb = 3;
+
+console.log(isJsProgrammingLanguage);
+console.log(isJsHard);
+console.log(favNumb + undefined);
