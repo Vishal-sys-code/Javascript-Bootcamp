297f6142f3c0b6b87a431bf4b5270699003ece6c
Vishal Pandey <bishalpandey2001@gmail.com>
2024-03-02 10:25:06 +0530

    Update main.js

--- 01. Beginners/02. Datatypes and Variables/02. Datatypes/Tutorial/main.js
+++ 01. Beginners/02. Datatypes and Variables/02. Datatypes/Tutorial/main.js
@@ -95,3 +95,24 @@ console.log("Lowercase lastName:", lastName.toLowerCase()); // Output: Lowercase
 // split
 let lastNameArray = lastName.split('');
 console.log("lastName split into an array:", lastNameArray); // Output: lastName split into an array: [ 'P', 'a', 'n', 'd', 'e', 'y' ]
+
+// --------------------------------------------
+
+/* TYPE CONVERSION */
+
+let amount = 100;
+let money = "100";
+let floatValue = "99.5";
+
+// Convert string to number
+amount = parseInt(amount);
+amount = +amount;
+amount = Number(amount);
+
+// Convert number to string
+money = money.toString();
+money = String(money);
+
+// Change string to decimal
+floatValue = parseFloat(floatValue);
+console.log(floatValue);
