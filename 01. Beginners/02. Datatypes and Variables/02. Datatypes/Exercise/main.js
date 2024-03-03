1ce594c5cf0ede0f5ef3c16aeb9129d62b2d1c59
Vishal Pandey <bishalpandey2001@gmail.com>
2024-03-03 14:58:17 +0530

    Update main.js

--- 01. Beginners/02. Datatypes and Variables/02. Datatypes/Exercise/main.js
+++ 01. Beginners/02. Datatypes and Variables/02. Datatypes/Exercise/main.js
@@ -1,4 +1,6 @@
+// --------------------------------------------------------------------------------
 /* NUMBERS */
+console.log("--------- NUMBERS ASSIGNMENTS ---------");
 let firstFavNum = 3;
 let secondFavNum = 29;
 let addNum = firstFavNum + secondFavNum;
@@ -15,7 +17,9 @@ console.log(`Division: ${divNum}`);
 console.log(`Mod Division: ${checkNum}`);
 console.log(`Power: ${power}`);
 
+// --------------------------------------------------------------------------------
 /* BOOLEANS */
+console.log("--------- BOOLEAN ASSIGNMENTS ---------");
 let isJsProgrammingLanguage = true;
 let isJsHard = false;
 let favNumb = 3;
@@ -23,3 +27,31 @@ let favNumb = 3;
 console.log(isJsProgrammingLanguage);
 console.log(isJsHard);
 console.log(favNumb + undefined);
+
+// --------------------------------------------------------------------------------
+/* COMPARISON OPERATORS */
+console.log("--------- COMPARISON OPERATORS ASSIGNMENTS ---------");
+
+firstFavNum = 3;
+secondFavNum = 29;
+
+console.log(`${firstFavNum > secondFavNum}`);
+console.log(`${firstFavNum < secondFavNum}`);
+console.log(`${firstFavNum >= secondFavNum}`);
+console.log(`${firstFavNum <= secondFavNum}`);
+console.log(`${firstFavNum === secondFavNum}`);
+console.log(`${firstFavNum == secondFavNum}`);
+console.log(`${firstFavNum !== secondFavNum}`);
+console.log(`${firstFavNum != secondFavNum}`);
+
+// --------------------------------------------------------------------------------
+/* STRING */
+console.log("--------- STRING ASSIGNMENTS ---------");
+let favActorFirstName = "Vishal";
+let favActorLastName = "Pandey";
+// let fullName = (`FullName: ${favActorFirstName + favActorLastName}`);
+fullName = favActorFirstName + " " + favActorLastName;
+let upperCase = fullName.toUpperCase();
+let message = (`My favorite Actor is ${upperCase} & He is a good man &`);
+message += " his best show is Silicon Valley";
+console.log(message);
