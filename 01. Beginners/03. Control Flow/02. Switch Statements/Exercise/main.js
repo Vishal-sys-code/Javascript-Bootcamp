7a9b16b3b36f20de367c729bb9a312b82b319f98
Vishal Pandey <bishalpandey2001@gmail.com>
2024-03-04 16:19:16 +0530

    Update main.js

---
+++ 01. Beginners/03. Control Flow/02. Switch Statements/Exercise/main.js
@@ -0,0 +1,14 @@
+let fruit = "Banana";
+switch(fruit){
+    case "Banana":
+        console.log(`Banana is good`);
+        break;
+    case "Orange":
+        console.log("I am not a fan of orange");
+        break;
+    case "Apple":
+        console.log(`How you like them apples?`);
+        break;
+    default:
+        console.log(`I have never heard of that fruit`);
+}
