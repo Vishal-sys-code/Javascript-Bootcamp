e2ef0e4c8a4dfb802e491a84372d918e3e54a78d
Vishal Pandey <bishalpandey2001@gmail.com>
2024-03-06 15:47:46 +0530

    Create main.js

---
+++ 01. Beginners/04. References Types/01. Arrays/Exercise/main.js
@@ -0,0 +1,11 @@
+const favSingers = ["Lana Del Rey", "Taylor Swift", "Selena Gomez"];
+console.log(`First Favorite Singer: ${favSingers[0]}`);
+
+const favNumbers = [3, 29, 22, 15];
+console.log(`First Favorite Number: ${favNumbers[0]}`);
+console.log(`First Favorite Number: ${favNumbers[1]}`);
+console.log(`First Favorite Number: ${favNumbers[2]}`);
+console.log(`First Favorite Number: ${favNumbers[3]}`);
+
+const mixedArr = ["string", ["otherArray"], 123, true];
+console.log(mixedArr);
