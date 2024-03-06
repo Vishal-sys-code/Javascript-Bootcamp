473ffb609ee6debf532056d57c7a4870722cfc70
Vishal Pandey <bishalpandey2001@gmail.com>
2024-03-06 11:28:13 +0530

    Update main.js

---
+++ 01. Beginners/04. References Types/01. Arrays/Tutorial/main.js
@@ -0,0 +1,30 @@
+/* 
+-------------------- ARRAYS --------------------
+- An array is an object that can store multiple values at once.
+
+Object allow you to store keyed collection of values.
+There exist a special data structure named Array, to store ordered collections.
+
+
+Arrays are a fundamental data structure in JavaScript that allow you to store and organize a collection of items under a single variable name. They are versatile and dynamic, meaning they can hold various data types like numbers, strings, objects, and even other arrays.
+
+There are two main ways to create arrays in Js:
+1. Array literal: This is the most common and concise wat, using sqaure brackets [] to enclose a comma-seperated list of elements.
+2. Array Constructor: While less common, you can also use the Array() constructor
+
+Declaration:
+let arr = new Array();
+let arr = [];
+
+*/
+
+/* EMPTY ARRAY */
+const myList = [];
+
+/* ARRAY OF MEMBERS */
+const numberArray = [2, 4, 6, 8];
+
+/* ARRAY OF STRINGS */
+const stringArray = ["eat", "sleep", "repeat"];
+
+/* ARRAY */
